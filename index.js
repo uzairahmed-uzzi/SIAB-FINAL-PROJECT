47d19ccd1184eb47176f4efca89d3e5cad7e4b1b
Uzair Ahmed <64686086+uzairahmed-uzzi@users.noreply.github.com>
2023-09-30 08:14:29 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
-// const bodyParser = require("body-parser")
+
 const app = express()
 const Routes = require("./routes/route.js")
 
@@ -10,8 +10,6 @@ const PORT = process.env.PORT || 5000
 
 dotenv.config();
 
-// app.use(bodyParser.json({ limit: '10mb', extended: true }))
-// app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
 app.use(express.json({ limit: '10mb' }))
 app.use(cors())
